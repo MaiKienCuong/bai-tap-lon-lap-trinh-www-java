54ba942d9bc628b2bdb49c8e0f7217b8a12f855e
maikiencuong <maikiencuongiuh@gmail.com>
2021-08-11 00:13:34 +0700

    thay doi SecurityContextHolder.getContext() thanh SecurityContextHolder.createEmptyContext();

--- src/main/java/quanaotreem/jwt/AuthTokenFilter.java
+++ src/main/java/quanaotreem/jwt/AuthTokenFilter.java
@@ -1,14 +1,8 @@
 package quanaotreem.jwt;
 
-import java.io.IOException;
-
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -16,49 +10,57 @@ import org.springframework.security.web.authentication.WebAuthenticationDetailsS
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 public class AuthTokenFilter extends OncePerRequestFilter {
 
-	@Autowired
-	private JwtUtils jwtUtils;
+    @Autowired
+    private JwtUtils jwtUtils;
 
-	@Autowired
-	private UserDetailsService userDetailsService;
+    @Autowired
+    private UserDetailsService userDetailsService;
 
-	@Override
-	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
-			throws ServletException, IOException {
-		String jwt = parseJwt(request);
-		if (jwt != null && jwtUtils.validateJwtToken(jwt)) {
-			String username = jwtUtils.getUserNameFromJwtToken(jwt);
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        String jwt = parseJwt(request);
+        if (jwt != null && jwtUtils.validateJwtToken(jwt)) {
+            String username = jwtUtils.getUserNameFromJwtToken(jwt);
 
-			UserDetails userDetails = userDetailsService.loadUserByUsername(username);
-			UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userDetails,
-					null, userDetails.getAuthorities());
-			authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-			SecurityContextHolder.getContext().setAuthentication(authentication);
-		}
+            UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+            UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userDetails,
+                    null, userDetails.getAuthorities());
+            authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+            SecurityContext context = SecurityContextHolder.createEmptyContext();
+            context.setAuthentication(authentication);
+            SecurityContextHolder.setContext(context);
+        }
 //		filterChain la 1 chuoi cac filter lien tiep nhau
-		filterChain.doFilter(request, response);
+        filterChain.doFilter(request, response);
 //		sau khi xac thuc xong, doFilter(request, response) dung de chuyen quyen dieu khien cho filter tiep theo trong chuoi filter
-	}
+    }
 
-	/**
-	 * Parses the jwt.
-	 * 
-	 * <p>
-	 * Lay Authorization header trong header cua request, tra ve chuoi jwt
-	 * </p>
-	 *
-	 * @param request the request
-	 * @return the string jwt
-	 */
-	private String parseJwt(HttpServletRequest request) {
-		String headerAuth = request.getHeader("Authorization");
+    /**
+     * Parses the jwt.
+     *
+     * <p>
+     * Lay Authorization header trong header cua request, tra ve chuoi jwt
+     * </p>
+     *
+     * @param request the request
+     * @return the string jwt
+     */
+    private String parseJwt(HttpServletRequest request) {
+        String headerAuth = request.getHeader("Authorization");
 
-		if (StringUtils.hasText(headerAuth) && headerAuth.startsWith("Bearer ")) {
-			return headerAuth.substring(7, headerAuth.length());
-		}
+        if (StringUtils.hasText(headerAuth) && headerAuth.startsWith("Bearer ")) {
+            return headerAuth.substring(7, headerAuth.length());
+        }
 
-		return null;
-	}
+        return null;
+    }
 }
